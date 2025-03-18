f8cde2d3551af9e2d560bd12c4dec33d83959322
LevanMakaradze <levanluk77@gmail.com>
2025-03-18 18:09:23 +0400

    finished design for Tasks page Task container

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -288,6 +288,29 @@ const Dashboard = () => {
     },
   ];
 
+  const statusColors = {
+    1: "#F7BC30", // Yellow
+    2: "#FB5607", // Orange
+    3: "#FF006E", // Pink
+    4: "#FF006E", // Blue
+  };
+
+  const priorityColors = {
+    1: "#08A508", //Green
+    2: "#FFBE0B", //Yellow
+    3: "#FA4D4D",
+  };
+
+  const departmentColors = {
+    0: "#ff66a8",
+    1: "#89b6ff",
+    2: "#ffd86d",
+    3: "#fd9a6a",
+    4: "#17f1fd",
+    5: "#41ff0c",
+    6: "#e14704",
+  };
+
   return (
     <>
       <div className={styles.mainContainer}>
@@ -360,59 +383,69 @@ const Dashboard = () => {
           ))}
         </div>
 
-        {/* Active Filters Display */}
-        {(filters.departments.length > 0 ||
-          filters.priorities.length > 0 ||
-          filters.employee) && (
-          <div className={styles.selectedFilters}>
-            {filters.departments.map((deptId) => (
-              <div
-                key={`active-dept-${deptId}`}
-                className={styles.selectedFilter}
-              >
-                {getDepartmentName(deptId)}
-                <button onClick={() => removeFilter("departments", deptId)}>
-                  <X size={14} style={{color:"#343A40"}}/>
-                </button>
-              </div>
-            ))}
+        <div className={styles.selectedFilters}>
+          {/* Active Filters Display */}
+          {(filters.departments.length > 0 ||
+            filters.priorities.length > 0 ||
+            filters.employee) && (
+            <>
+              {filters.departments.map((deptId) => (
+                <div
+                  key={`active-dept-${deptId}`}
+                  className={styles.selectedFilter}
+                >
+                  {getDepartmentName(deptId)}
+                  <button onClick={() => removeFilter("departments", deptId)}>
+                    <X size={14} style={{ color: "#343A40" }} />
+                  </button>
+                </div>
+              ))}
 
-            {filters.priorities.map((priorityId) => (
-              <div
-                key={`active-priority-${priorityId}`}
-                className={styles.selectedFilter}
-              >
-                {getPriorityName(priorityId)}
-                <button onClick={() => removeFilter("priorities", priorityId)}>
-                <X size={14} style={{color:"#343A40"}}/>
-                </button>
-              </div>
-            ))}
+              {filters.priorities.map((priorityId) => (
+                <div
+                  key={`active-priority-${priorityId}`}
+                  className={styles.selectedFilter}
+                >
+                  {getPriorityName(priorityId)}
+                  <button
+                    onClick={() => removeFilter("priorities", priorityId)}
+                  >
+                    <X size={14} style={{ color: "#343A40" }} />
+                  </button>
+                </div>
+              ))}
 
-            {filters.employee && (
-              <div className={styles.selectedFilter}>
-                {getEmployeeName(filters.employee)}
-                <button onClick={() => removeFilter("employee")}>
-                <X size={14} style={{color:"#343A40"}}/>
-                </button>
-              </div>
-            )}
+              {filters.employee && (
+                <div className={styles.selectedFilter}>
+                  {getEmployeeName(filters.employee)}
+                  <button onClick={() => removeFilter("employee")}>
+                    <X size={14} style={{ color: "#343A40" }} />
+                  </button>
+                </div>
+              )}
 
-            <button onClick={clearAllFilters} className={styles.filterClear}>გასუფთავება</button>
-          </div>
-        )}
+              <button onClick={clearAllFilters} className={styles.filterClear}>
+                გასუფთავება
+              </button>
+            </>
+          )}
+        </div>
 
         {/* Filtered Tasks Display */}
-        <div style={{ marginTop: "30px" }}>
+        <div>
           {loading && <p>Loading...</p>}
           {error && <p>{error}</p>}
-          <div style={{ display: "flex", gap: "20px" }}>
+
+          <div className={styles.tasksContainer}>
             {statuses.map((status) => (
-              <div
-                key={status.id}
-                style={{ width: "25%", height: "800px", overflowY: "scroll" }}
-              >
-                <span style={{ color: "red" }}>{status.name}</span>
+              <div key={status.id} className={styles.tasksColumns}>
+                <span
+                  className={styles.statusTitle}
+                  style={{ backgroundColor: statusColors[status.id] }}
+                >
+                  {status.name}
+                </span>
+
                 {filteredTasks
                   .filter((task) => task.status.id === status.id)
                   .map((task) => (
@@ -420,28 +453,101 @@ const Dashboard = () => {
                       to={`/task/${task.id}`}
                       style={{ textDecoration: "none", color: "inherit" }}
                     >
-                      <div
-                        key={task.id}
-                        style={{
-                          border: "1px solid black",
-                          padding: "10px",
-                          marginTop: "10px",
-                        }}
-                      >
-                        <h4>{task.name}</h4>
-                        <p>
-                          {task.description.length > 100
-                            ? `${task.description.substring(0, 100)}...`
-                            : task.description}
-                        </p>
-                        <p>{task.department.name}</p>
-                        <p>{task.priority.name}</p>
-                        <img
-                          src={task.employee.avatar}
-                          alt="avatar"
-                          style={{ width: "20px", height: "20px" }}
-                        />
-                        <p>კომენტარები: {task.total_comments}</p>
+                      <div key={task.id} className={styles.taskCard}>
+                        <div className={styles.taskCardTop}>
+                          <div className={styles.taskCardTopLeft}>
+                            <span
+                              className={styles.taskCardPriority}
+                              style={{
+                                borderColor: priorityColors[task.priority.id],
+                                color: priorityColors[task.priority.id],
+                              }}
+                            >
+                              <img
+                                src={task.priority.icon}
+                                width="18px"
+                                height="20px"
+                                style={{ marginRight: "4px" }}
+                              />
+                              {task.priority.name}
+                            </span>
+
+                            <span
+                              className={styles.taskCardDepartment}
+                              style={{
+                                backgroundColor:
+                                  departmentColors[task.department.id],
+                              }}
+                            >
+                              {task.department.name}
+                            </span>
+                          </div>
+                          <div className={styles.taskCardTopRight}>
+                            <span className={styles.taskCardDate}>
+                              {(() => {
+                                const months = [
+                                  "იან",
+                                  "თებ",
+                                  "მარ",
+                                  "აპრ",
+                                  "მაი",
+                                  "ივნ",
+                                  "ივლ",
+                                  "აგვ",
+                                  "სექ",
+                                  "ოქტ",
+                                  "ნოე",
+                                  "დეკ",
+                                ];
+
+                                const dateString = task.due_date;
+
+                                const date = new Date(dateString);
+                                const day = date.getDate();
+                                const month = months[date.getMonth()];
+                                const year = date.getFullYear();
+
+                                return `${day} ${month}, ${year}`;
+                              })()}
+                            </span>
+                          </div>
+                        </div>
+
+                        <div className={styles.taskCardMiddle}>
+                          <span className={styles.taskTitle}>{task.name}</span>
+                          <p className={styles.taskDescription}>
+                            {task.description.length > 100
+                              ? `${task.description.substring(0, 100)}...`
+                              : task.description}
+                          </p>
+                        </div>
+
+                        <div className={styles.taskCardBottom}>
+                          <img
+                            src={task.employee.avatar}
+                            alt="avatar"
+                            style={{
+                              width: "31px",
+                              height: "31px",
+                              borderRadius: "50%",
+                            }}
+                          />
+                          <div className={styles.taskCardComments}>
+                            <svg
+                              width="22"
+                              height="23"
+                              viewBox="0 0 22 23"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M3.08086 2.25977C1.87258 2.25977 0.880859 3.25148 0.880859 4.45977V15.0198C0.880859 16.228 1.87258 17.2198 3.08086 17.2198H4.88211C4.94227 17.7491 4.93539 18.239 4.79961 18.6498C4.63289 19.1551 4.3218 19.5796 3.74086 19.9285C3.57758 20.0316 3.50195 20.2293 3.5518 20.4149C3.60164 20.6005 3.76836 20.7329 3.96086 20.7398C5.82742 20.7398 7.96727 19.7652 9.04836 17.2198H18.9209C20.1291 17.2198 21.1209 16.228 21.1209 15.0198V4.45977C21.1209 3.25148 20.1291 2.25977 18.9209 2.25977H3.08086ZM3.08086 3.13977H18.9209C19.6496 3.13977 20.2409 3.73102 20.2409 4.45977V15.0198C20.2409 15.7485 19.6496 16.3398 18.9209 16.3398H8.80086C8.61695 16.3398 8.45195 16.4549 8.38836 16.6285C7.7043 18.4951 6.48227 19.3837 5.21211 19.7085C5.38398 19.4627 5.54727 19.2032 5.63836 18.9248C5.86695 18.2304 5.84805 17.4707 5.70711 16.6973C5.66758 16.4927 5.49055 16.3432 5.28086 16.3398H3.08086C2.35211 16.3398 1.76086 15.7485 1.76086 15.0198V4.45977C1.76086 3.73102 2.35211 3.13977 3.08086 3.13977Z"
+                                fill="#212529"
+                              />
+                            </svg>
+                            <span>{task.total_comments}</span>
+                          </div>
+                        </div>
                       </div>
                     </NavLink>
                   ))}
